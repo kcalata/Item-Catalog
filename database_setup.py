dde81cce52632f9fe0a6d3467371a17093270dba
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-13 21:53:28 -0400

    Add Category and Item classes

---
+++ database_setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declartive_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declartive_base()
+
+
+class Category(Base):
+    __table__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+
+
+Base.metadata.create_all(engine)
