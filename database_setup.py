fd5f2471a4f51d046e10c9ced9d8b44502b5b99e
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-13 21:59:07 -0400

    Add serialize functions to database objects

--- database_setup.py
+++ database_setup.py
@@ -14,6 +14,14 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
 
 class Item(Base):
     __tablename__ = 'item'
@@ -23,6 +31,15 @@ class Item(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id
+        }
 
 
 engine = create_engine('sqlite:///itemcatalog.db')
