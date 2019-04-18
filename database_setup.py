3991195fa61ce7cee53027f67e5034f129d50c9b
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-17 22:17:11 -0400

    Change primary key of category

--- database_setup.py
+++ database_setup.py
@@ -11,8 +11,7 @@ Base = declarative_base()
 class Category(Base):
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False, primary_key=True)
 
     @property
     def serialize(self):
@@ -29,7 +28,7 @@ class Item(Base):
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    category_id = Column(Integer, ForeignKey('category.id'))
+    category_name = Column(String, ForeignKey('category.name'))
     category = relationship(Category)
 
     @property
