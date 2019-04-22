d4c977eb381c72b779ce4b9daf3c8a2d53b1d3cd
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-21 21:12:04 -0400

    Add User class

--- database_setup.py
+++ database_setup.py
@@ -8,6 +8,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
 
